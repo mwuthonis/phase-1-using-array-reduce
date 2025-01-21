5ee252cb1c4372f84b0f66c76903f5298bb278ec
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-01-21 11:32:52 +0300

    Creatinging Single Aggregate of All items

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+//Using reduce to sum up all the battery counts
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+
+console.log(totalBatteries);
+
 // Code your solution here
